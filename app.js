e80fa53650b6b92dd2d37b22ecbd916ac6120065
Zhuohan Song <zhuohansong886@Zhuohans-MacBook-Air.local>
2022-11-28 19:41:00 -0500

    a9 mongo

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ import UserController
 import tuitsController from './controllers/tuits/tuits-controller.js'
 import express from 'express'
 import cors from 'cors'
+import mongoose from "mongoose";
 const app = express()
 app.use(cors())
 app.use(express.json());
 HelloController(app)
 UserController(app)
 tuitsController(app)
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb://localhost:27017/tuiter');
+app.get('/env', (req,res)=> res.send(process.env.DB_CONNECTION_STRING))
 app.listen(process.env.PORT||4000)
