6c3bb790acb5d43f683eb3cf95f6b452bacea2b1
RealHape <68569037+SSSZH886@users.noreply.github.com>
2022-11-30 14:53:10 -0500

    Update tuits.js

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -3,18 +3,25 @@ export default [
      "_id": "123", "topic": "Space", "username": "SpaceX",
      "handle": "@spacex", "time": "2h", "image": "booster.jpg",
      "title": "SpaceX's Mission",
-     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-     "liked": true, "likes": 2345,
+     "tuit": "Tesla stock falls below 200",
+     "liked": false, "likes": 0,
      "replies": 123, "retuits": 432
     },
     {
      "_id": "234", "topic": "Traffic", "username": "The Boring Company",
      "handle": "@boringcompany", "time": "2h", "image": "react.jpg",
      "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "likes": 2345,
+     "tuit": "Starlink likely to go public",
+     "liked": true, "likes": 42069,
      "replies": 123, "retuits": 432
     },
+    {
+        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
+        "handle": "@boringcompany", "time": "2h", "image": "react.jpg",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "tuit": "Model Y is most American made car",
+        "liked": true, "likes": 69420,
+        "replies": 123, "retuits": 432
+       },
     ];
-    
     
